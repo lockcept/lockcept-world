a4dda6ec7e58dde766408f560f2ac92ebe265018
lockcept <lockcept@gmail.com>
2020-10-15 19:45:34 +0900

    feat(front-end): link shared and add Logger

---
+++ packages/front-end/src/logger/index.ts
@@ -0,0 +1,4 @@
+export const errorLogger = (e: Error) => {
+  // eslint-disable-next-line no-console
+  console.log(e);
+};
--- packages/front-end/src/sign/signup.tsx
+++ packages/front-end/src/sign/signup.tsx
@@ -1,15 +1,39 @@
 import { Typography } from "@material-ui/core";
 import { AxiosInstance } from "axios";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { CreateUserRequest, CreateUserResponse } from "@lockcept/shared";
+import { errorLogger } from "../logger";
 
 interface Props {
   instance: AxiosInstance;
 }
 
-function Signup({ instance: _instance }: Props) {
+function Signup({ instance }: Props) {
+  const [ifCreated, setIfCreated] = useState<boolean>(false);
+  const [ifExist, setIfExist] = useState<boolean>(false);
+
+  const signup = useCallback(async () => {
+    const userData = {
+      id: "lockcept",
+      password: "lockcept",
+      nickname: "lockcept",
+    };
+    const reqBody: CreateUserRequest = { userData };
+    try {
+      const res = await instance.post<CreateUserResponse>("/users", reqBody);
+      setIfCreated(res.data.ifCreated);
+    } catch (e) {
+      errorLogger(e);
+    }
+  }, [instance]);
+
+  useEffect(() => {
+    signup();
+  }, [signup]);
+
   return (
     <>
-      <Typography>Signup</Typography>
+      <Typography>{JSON.stringify(ifCreated)}</Typography>
     </>
   );
 }
