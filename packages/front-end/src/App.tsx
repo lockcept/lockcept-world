89deae73bb48101a08fce7ed4131937dab9faee3
lockcept <lockcept@gmail.com>
2020-12-15 01:46:33 +0900

    chore(back-end, front-end): add stg STAGE

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -13,12 +13,15 @@ const getHttpEndPoints = () => {
   const httpEndPoints = {
     dev: "http://localhost:4000",
     prod: "https://api.lockcept.kr/prod",
+    stg: "https://api.lockcept.kr/stg",
   };
   const stage = process.env.REACT_APP_STAGE;
   if (!stage) return httpEndPoints.dev;
   switch (stage) {
     case "prod":
       return httpEndPoints.prod;
+    case "stg":
+      return httpEndPoints.stg;
     default:
       return httpEndPoints.dev;
   }
--- readme.md
+++ readme.md
@@ -13,6 +13,7 @@ web browser application
 typescript, react, material-ui, AWS S3 static  
 `yarn start` for dev (localhost:3000)  
 `yarn deploy:prod` for prod
+`yarn deploy:stg` for stg
 
 ### back-end
 
@@ -20,6 +21,7 @@ api server application
 typescript, serverless, AWS APIGateway & lambda, dynamodb  
 `yarn dev` for dev  
 `yarn deploy:prod` for prod
+`yarn deploy:stg` for stg
 
 ### shared
 
