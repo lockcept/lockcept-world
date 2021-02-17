be16aa935ce2195f2e35b56fffb0f5b72895c71b
lockcept <lockcept@gmail.com>
2021-02-18 04:21:44 +0900

    feat(front-end): log-out

--- packages/front-end/src/containers/main/MainHeader.tsx
+++ packages/front-end/src/containers/main/MainHeader.tsx
@@ -5,11 +5,13 @@ import {
   createStyles,
   Link,
   makeStyles,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@material-ui/core";
-import React, { useMemo } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useMemo, useState } from "react";
+import { useHistory, Link as ReactLink } from "react-router-dom";
 import { useLockceptContext } from "../../contexts";
 
 const useStyles = makeStyles(() =>
@@ -24,7 +26,12 @@ const MainHeader = () => {
   const classes = useStyles();
   const history = useHistory();
 
-  const { signed, signedUserData } = useLockceptContext();
+  const { signed, signedUserData, setAccessToken } = useLockceptContext();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const userName = useMemo(() => {
     if (!signedUserData) return "";
     return signedUserData?.userName;
@@ -53,7 +60,41 @@ const MainHeader = () => {
               Sign-in
             </Button>
           )}
-          {signed && userName}
+          {signed && (
+            <Button
+              color="inherit"
+              onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                setAnchorEl(event.currentTarget);
+              }}
+            >
+              {userName}
+            </Button>
+          )}
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem
+              component={ReactLink}
+              to="/account"
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              Account Setting
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                setAccessToken("");
+              }}
+            >
+              Log out
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
